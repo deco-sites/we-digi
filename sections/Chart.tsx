23903c01cdbb10560ca899433d66609ae794c053
RyanSantos <38843412+RyanSantosRibeiro@users.noreply.github.com>
2024-09-25 12:20:23 -0300

    fix: chart

--- sections/Chart.tsx
+++ sections/Chart.tsx
@@ -8,14 +8,26 @@ export interface CTA {
   href: string;
   text: string;
   type?: 'default' | 'outline' | 'inline';
+  outline: boolean;
 }
 
 interface DescriptionList {
+  /**
+   * @title Title
+   */
+  title: string;
   /**
    * @title Description
    * @format rich-text
    */
   label: string;
+
+  color: string;
+
+  /**
+   * @title Gráfico
+   */
+  image?: ImageWidget;
 }
 
 export interface Props {
@@ -32,10 +44,6 @@ export interface Props {
    */
   description?: DescriptionList[];
   /**
-   * @title Gráfico
-   */
-  image?: ImageWidget;
-  /**
    * @title Imagem de Fundo
    */
   background?: ImageWidget;
@@ -48,7 +56,6 @@ export default function Chart({
   title = "we.digi",
   tag,
   background,
-  image,
   description = [],
   cta = [
     { id: "change-me-1", href: "/", text: "CONHEÇA NOSSOS SERVIÇOS", outline: false },
@@ -119,30 +126,53 @@ export default function Chart({
       let currentIndex = 0; // Índice inicial
 
     // Função para trocar o texto com animações
-    function changeText() {
+    function changeText(index: number) {
       const element = document.getElementById("textElement");
-      if(!element) return;
+      const title = element?.querySelector('.textElement__title') as HTMLElement;
+      const label = element?.querySelector('span');
+      const chart = document?.getElementById('chart-image') as HTMLElement;
+      if(!element || !label || !title) return;
 
       // Aplicar animação de fade-out
       element.classList.remove("fade-in");
       element.classList.add("fade-out");
+      
+      chart.classList.remove("fade-in");
+      chart.classList.add("fade-out");
+
 
       // Esperar a animação de fade-out terminar (1 segundo)
       setTimeout(() => {
         // Trocar o HTML usando dangerouslySetInnerHTML
-        element.innerHTML = texts[currentIndex].label;
-
+        console.log(texts[index].image)
+        chart.style.backgroundImage = `url(${texts[index].image})`;
+        label.innerHTML = texts[index].label;
+        title.innerHTML = texts[index].title;
+        title.style.color = texts[index].color
         // Alternar para o próximo índice, com reset para o início do array
         currentIndex = (currentIndex + 1) % texts.length;
 
         // Aplicar animação de fade-in
         element.classList.remove("fade-out");
         element.classList.add("fade-in");
-      }, 1000); // Tempo igual à duração da animação fade-out
+        chart.classList.remove("fade-out");
+        chart.classList.add("fade-in");
+      }, 500); // Tempo igual à duração da animação fade-out
     }
 
-    // Chamar a função a cada 2 segundos
-    setInterval(changeText, 6000);
+    // Seleciona todos os botões com a classe 'btn'
+    const buttons = document.querySelectorAll('.chart__actions');
+
+    // Adiciona um event listener para cada botão
+    buttons.forEach(button => {
+      button.addEventListener('click', function(e:any) {
+          const index = e.target.getAttribute('data-index'); // Pega o valor do data-index do botão clicado
+          changeText(index); // Chama a função passando o index
+      });
+   });
+
+    // Chamar a função a cada 2 segundos Descomentar
+    // setInterval(changeText, 6000);
     });
 
   return (
@@ -170,14 +200,25 @@ export default function Chart({
                 </a>
               ))}
             </div>
-            <div class="flex items-center gap-3 mt-6 md:mt-32 lg:ml-4">
+            <div class="flex flex-col items-start justify-center gap-3 mt-6 md:mt-32 lg:ml-4">
+              <div class="flex justify-start items-center gap-2 w-full">
+                {description.map(({color,title},index)=>{
+                  return (
+                    <div class="chart__actions px-2 py-1 cursor-pointer" style={{border: `1px solid ${color}`,color: color}} data-index={index}>{title}</div>
+                  )
+                })}
+              </div>
               <div
                 id="textElement"
-                class="bg-[#153042] mb-8 max-w-[465px] [&_span]:[line-height:24px] [&_span]:[display:flex] p-4 [&_ul]:[list-style:disc] [&_ul]:[padding:0px_12px]"
+                class="bg-[#153042] mb-8 max-w-[465px] [&_span]:[line-height:24px] [&_span]:[display:flex] p-4 [&_ul]:[list-style:disc] [&_ul]:[padding:0px_12px] flex flex-col"
+                
+              >
+                <p class="textElement__title" style={{color: description[0].color}}>{description[0].title}</p>
+                <span class="flex-col"
                 dangerouslySetInnerHTML={{
                   __html: description[0].label,
-                }}
-              />
+                }} ></span>
+              </div>
               <script
               type="module"
               defer
@@ -189,16 +230,11 @@ export default function Chart({
         </div>
 
         {/* Bloco de Imagem */}
-        <div class="lg:w-1/2 mt-2 lg:mt-0">
-          {image && <Image
-              width={640}
-              class="w-full h-auto object-cover"
-              sizes="(max-width: 640px) 100vw, 50vw"
-              src={image}
-              alt="Imagem do Quadrante 2024"
-              decoding="async"
-              loading="lazy"
-            />}
+        <div class="lg:w-1/2 mt-2 lg:mt-0 flex justify-center items-center aspect-square">
+          <div id="chart-image" style={{backgroundImage: `url(${description[0].image})`, backgroundSize: '100% auto', backgroundPosition: 'center'}} class="w-[485px] max-w-full h-[485px] max-h-full transition-all">
+            
+          </div>
+          
           {/* <div>
              To input a chart
             <canvas id="myChart"></canvas>
