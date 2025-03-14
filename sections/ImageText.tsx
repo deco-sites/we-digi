2b0f64a2e21a8b7b267f268c7ee08350646eb7fb
Cleyton Mendes <cleyton.mendes@wedigi.com.br>
2025-03-14 17:17:21 -0300

    feat(pages): Páginas

--- sections/ImageText.tsx
+++ sections/ImageText.tsx
@@ -65,7 +65,7 @@ const ImageText = ({ text, image, bgColor, cta, invert }: Props) => {
             </div>
             <div class="w-full flex items-center justify-center">
                 <div class="lg:w-4/6 flex flex-col items-center lg:items-start">
-                    <div class="max-[1024px]:p-4 lg:[&_h2]:text-5xl lg:[&_h2]:mb-8 [&_h2]:mb-4 [&_h2]:text-4xl lg:[&_h3]:text-3xl [&_h3]:text-2xl [&_h3]:font-light lg:[&_h3]:mb-6 [&_h3]:mb-3 lg:[&_p]:!text-lg [&_p]:!text-sm [&_p]:!font-light [&_ul]:list-disc [&_ul]:pl-8 lg:[&_ul_span]:!text-lg [&_ul_span]:!text-sm" dangerouslySetInnerHTML={{ __html: text }} />
+                    <div class="max-[1024px]:p-4 lg:[&_h2]:text-5xl lg:[&_h2]:mb-8 [&_h2]:mb-4 [&_h2]:text-4xl lg:[&_h3]:text-3xl [&_h3]:text-2xl [&_h3]:font-light lg:[&_h3]:mb-6 [&_h3]:mb-3 lg:[&_p]:!text-lg [&_p]:!text-sm [&_p]:!font-light [&_ul]:list-disc [&_ul]:pl-8 lg:[&_ul_span]:!text-lg [&_ul_span]:!text-sm [&_p]:!my-3" dangerouslySetInnerHTML={{ __html: text }} />
                     {cta?.url &&
                         <a class="block w-fit mt-4 px-5 py-1 rounded-[50px] font-light text-base lg:text-xl" style={{ backgroundColor: cta.bgColor ?? '#FFFFFF', color: cta.textColor ?? '#000000',}} href={cta.url}>{cta?.text ?? 'Botão'}</a>
                     }
