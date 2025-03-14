a95bafeed541eb5f4eba43e594bfffa0b81c20e5
Cleyton Mendes <cleyton.mendes@wedigi.com.br>
2025-03-14 17:49:59 -0300

    feat(FAQ): faq image da página performance

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -13,19 +13,20 @@ import * as $$$$$$2 from "./sections/Cases.tsx";
 import * as $$$$$$3 from "./sections/Chart.tsx";
 import * as $$$$$$4 from "./sections/Contact.tsx";
 import * as $$$$$$5 from "./sections/Faq.tsx";
-import * as $$$$$$6 from "./sections/Footer.tsx";
-import * as $$$$$$7 from "./sections/Header.tsx";
-import * as $$$$$$16 from "./sections/hero-1.tsx";
-import * as $$$$$$17 from "./sections/hero-2.tsx";
-import * as $$$$$$8 from "./sections/Hero.tsx";
-import * as $$$$$$18 from "./sections/hero2.tsx";
-import * as $$$$$$9 from "./sections/ImageText.tsx";
-import * as $$$$$$10 from "./sections/ImageWithParagraph.tsx";
-import * as $$$$$$11 from "./sections/Logos.tsx";
-import * as $$$$$$12 from "./sections/Partnerships.tsx";
-import * as $$$$$$13 from "./sections/RichText.tsx";
-import * as $$$$$$14 from "./sections/Testimonials.tsx";
-import * as $$$$$$15 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$6 from "./sections/FaqImage.tsx";
+import * as $$$$$$7 from "./sections/Footer.tsx";
+import * as $$$$$$8 from "./sections/Header.tsx";
+import * as $$$$$$17 from "./sections/hero-1.tsx";
+import * as $$$$$$18 from "./sections/hero-2.tsx";
+import * as $$$$$$9 from "./sections/Hero.tsx";
+import * as $$$$$$19 from "./sections/hero2.tsx";
+import * as $$$$$$10 from "./sections/ImageText.tsx";
+import * as $$$$$$11 from "./sections/ImageWithParagraph.tsx";
+import * as $$$$$$12 from "./sections/Logos.tsx";
+import * as $$$$$$13 from "./sections/Partnerships.tsx";
+import * as $$$$$$14 from "./sections/RichText.tsx";
+import * as $$$$$$15 from "./sections/Testimonials.tsx";
+import * as $$$$$$16 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
@@ -35,19 +36,20 @@ const manifest = {
     "site/sections/Chart.tsx": $$$$$$3,
     "site/sections/Contact.tsx": $$$$$$4,
     "site/sections/Faq.tsx": $$$$$$5,
-    "site/sections/Footer.tsx": $$$$$$6,
-    "site/sections/Header.tsx": $$$$$$7,
-    "site/sections/hero-1.tsx": $$$$$$16,
-    "site/sections/hero-2.tsx": $$$$$$17,
-    "site/sections/Hero.tsx": $$$$$$8,
-    "site/sections/hero2.tsx": $$$$$$18,
-    "site/sections/ImageText.tsx": $$$$$$9,
-    "site/sections/ImageWithParagraph.tsx": $$$$$$10,
-    "site/sections/Logos.tsx": $$$$$$11,
-    "site/sections/Partnerships.tsx": $$$$$$12,
-    "site/sections/RichText.tsx": $$$$$$13,
-    "site/sections/Testimonials.tsx": $$$$$$14,
-    "site/sections/Theme/Theme.tsx": $$$$$$15,
+    "site/sections/FaqImage.tsx": $$$$$$6,
+    "site/sections/Footer.tsx": $$$$$$7,
+    "site/sections/Header.tsx": $$$$$$8,
+    "site/sections/hero-1.tsx": $$$$$$17,
+    "site/sections/hero-2.tsx": $$$$$$18,
+    "site/sections/Hero.tsx": $$$$$$9,
+    "site/sections/hero2.tsx": $$$$$$19,
+    "site/sections/ImageText.tsx": $$$$$$10,
+    "site/sections/ImageWithParagraph.tsx": $$$$$$11,
+    "site/sections/Logos.tsx": $$$$$$12,
+    "site/sections/Partnerships.tsx": $$$$$$13,
+    "site/sections/RichText.tsx": $$$$$$14,
+    "site/sections/Testimonials.tsx": $$$$$$15,
+    "site/sections/Theme/Theme.tsx": $$$$$$16,
   },
   "apps": {
     "site/apps/deco/ai-assistants.ts": $$$$$$$$$$$0,
