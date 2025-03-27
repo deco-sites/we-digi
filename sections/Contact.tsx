e26f789d358b5f3818551f7f7f5a0f8593bf5006
Cleyton Mendes <cleyton.mendest@gmail.com>
2025-03-27 17:00:16 -0300

    feat(contact): bg branco

--- sections/Contact.tsx
+++ sections/Contact.tsx
@@ -64,7 +64,7 @@ export default function Footer({
           {description && <p class="text-white lg:text-2xl text-base">{description}</p>}
         </div>
         {/* <ContactForm /> */}
-        <div class="w-full md:w-[52%] contact-form-container" id="contact_form" />
+        <div class="w-full md:w-[52%] px-5 bg-white" id="contact_form" />
       </div>
     </div>
   );
