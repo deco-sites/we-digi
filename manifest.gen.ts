83a93c8f7b33fcdc945a0b05edab0fa5ead4d8e7
Cleyton Mendes <cleyton.mendest@gmail.com>
2025-03-28 16:13:57 -0300

    feat(lp-moda): inclui section textwithcard

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -18,10 +18,10 @@ import * as $$$$$$7 from "./sections/FaqImage.tsx";
 import * as $$$$$$8 from "./sections/Footer.tsx";
 import * as $$$$$$9 from "./sections/GridContent.tsx";
 import * as $$$$$$10 from "./sections/Header.tsx";
-import * as $$$$$$20 from "./sections/hero-1.tsx";
-import * as $$$$$$21 from "./sections/hero-2.tsx";
+import * as $$$$$$21 from "./sections/hero-1.tsx";
+import * as $$$$$$22 from "./sections/hero-2.tsx";
 import * as $$$$$$11 from "./sections/Hero.tsx";
-import * as $$$$$$22 from "./sections/hero2.tsx";
+import * as $$$$$$23 from "./sections/hero2.tsx";
 import * as $$$$$$12 from "./sections/ImageText.tsx";
 import * as $$$$$$13 from "./sections/ImageWithParagraph.tsx";
 import * as $$$$$$14 from "./sections/Logos.tsx";
@@ -29,7 +29,8 @@ import * as $$$$$$15 from "./sections/Partnerships.tsx";
 import * as $$$$$$16 from "./sections/RichText.tsx";
 import * as $$$$$$17 from "./sections/SimpleHeader.tsx";
 import * as $$$$$$18 from "./sections/Testimonials.tsx";
-import * as $$$$$$19 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$19 from "./sections/TextWithCards.tsx";
+import * as $$$$$$20 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
@@ -44,10 +45,10 @@ const manifest = {
     "site/sections/Footer.tsx": $$$$$$8,
     "site/sections/GridContent.tsx": $$$$$$9,
     "site/sections/Header.tsx": $$$$$$10,
-    "site/sections/hero-1.tsx": $$$$$$20,
-    "site/sections/hero-2.tsx": $$$$$$21,
+    "site/sections/hero-1.tsx": $$$$$$21,
+    "site/sections/hero-2.tsx": $$$$$$22,
     "site/sections/Hero.tsx": $$$$$$11,
-    "site/sections/hero2.tsx": $$$$$$22,
+    "site/sections/hero2.tsx": $$$$$$23,
     "site/sections/ImageText.tsx": $$$$$$12,
     "site/sections/ImageWithParagraph.tsx": $$$$$$13,
     "site/sections/Logos.tsx": $$$$$$14,
@@ -55,7 +56,8 @@ const manifest = {
     "site/sections/RichText.tsx": $$$$$$16,
     "site/sections/SimpleHeader.tsx": $$$$$$17,
     "site/sections/Testimonials.tsx": $$$$$$18,
-    "site/sections/Theme/Theme.tsx": $$$$$$19,
+    "site/sections/TextWithCards.tsx": $$$$$$19,
+    "site/sections/Theme/Theme.tsx": $$$$$$20,
   },
   "apps": {
     "site/apps/deco/ai-assistants.ts": $$$$$$$$$$$0,
---
+++ sections/TextWithCards.tsx
@@ -0,0 +1,66 @@
+import { ImageWidget } from "apps/admin/widgets.ts";
+/**@title {{title}} */
+interface Card {
+    /**
+     * @title ìcone
+     */
+    icon: ImageWidget
+    /**
+     * @title Título
+     */
+    title: string
+    /**
+     * @title Texto
+     * @format textarea
+     */
+    text: string
+    link: string
+}
+
+interface Props {
+    /**
+     * @title Texto
+     * @format rich-text
+     */
+    text: string
+    cards: Card[]
+}
+
+interface CardProps {
+    card: Card
+}
+
+const Card = ({ card }: CardProps) => {
+
+    return (
+        <div class="rounded-xl overflow-hidden md:flex-1 flex flex-col max-w-96">
+            <div class="bg-[#46c2ee] flex flex-col items-center p-4">
+                <img src={card.icon} alt={card.title} />
+                <h3 class="lg:text-xl text-[#292D6B] font-bold text-center">{card.title}</h3>
+            </div>
+            <div class="bg-white py-5 px-4 flex-1 flex flex-col items-center justify-center">
+                <div class="h-full flex justify-center items-center">
+                    <p class="text-black text-sm mb-8">{card.text}</p>
+                </div>
+                <a class="text-black bg-[#f0857d] text-center text-sm font-bold w-full block px-8 py-3 rounded-xl hover:!brightness-75 transition-all duration-300 ease-in-out" href={card.link}>TENHO INTERESSE</a>
+            </div>
+        </div>
+    )
+}
+
+const TextWithCards = ({ cards, text }: Props) => {
+    return (
+        <div class="bg-[#F2F2F2] ">
+            <div class="flex flex-col xl:flex-row py-16 gap-8 max-w-[94vw] mx-auto items-center">
+                <div class="flex-1">
+                    <div class="lg:[&_h2]:text-[40px] lg:[&_h2]:leading-[48px] [&_h2]:text-xl lg:[&_p]:text-base" dangerouslySetInnerHTML={{ __html: text }} />
+                </div>
+                <div class="flex-1 flex gap-8 flex-wrap md:flex-row flex-col md:justify-center">
+                    {cards?.map((card, index) => (<Card card={card} key={index} />))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TextWithCards
