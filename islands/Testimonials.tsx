644be9723699fe388aa11aeea12abcedf785a537
Cleyton Mendes <cleyton.mendest@gmail.com>
2025-04-03 14:28:39 -0300

    fix: correcao slider testimonials

---
+++ islands/Testimonials.tsx
@@ -0,0 +1,8 @@
+import Component from "../components/Testimonials.tsx";
+import { Props } from '../components/Testimonials.tsx'
+
+function Island(props: Props) {
+    return <Component {...props} />;
+}
+
+export default Island;
