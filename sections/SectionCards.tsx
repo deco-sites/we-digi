41cc47e3e5c2be1c96463680d265f7d8770e882c
Cleyton Mendes <cleyton.mendest@gmail.com>
2025-05-13 17:24:43 -0300

    style(sectionCards): ajustes de css

--- sections/SectionCards.tsx
+++ sections/SectionCards.tsx
@@ -24,8 +24,8 @@ const SectionCards = ({ bgColor, cards }: Props) => {
         <div style={bgColor ? { backgroundColor: bgColor } : {}}>
             <div class="flex flex-col md:flex-row mx-auto max-w-[1514px] gap-8 lg:py-12 py-4 px-4">
                 {cards.map((card, index) => (
-                    <div>
-                        <div class="w-full lg:max-w-[485px] rounded-[10px] h-[204px] overflow-hidden">
+                    <div class="w-full lg:max-w-[485px]">
+                        <div class="w-full rounded-[10px] h-[204px] overflow-hidden">
                             <img class="w-full h-full object-cover" src={card.image} alt={card.title} />
                         </div>
                         <div class="flex justify-center items-center gap-4 lg:mt-6 mt-2">
